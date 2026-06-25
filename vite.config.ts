d232cf87c2281cdcaebd1e7e3c03e37c00c6b6bd
Mujeeb Ur Rehman <bcs.23440216@imsciences.edu.pk>
2026-06-25 09:45:57 +0500

    fix: override nitro deployment preset target to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,8 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  // Pass additional target parameters to the underlying Nitro compilation compiler
+  nitro: {
+    preset: "vercel",
+  },
 });
