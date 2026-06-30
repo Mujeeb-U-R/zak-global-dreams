4c0377e3893fe8f4c148f9df6b9f88ab33060e8a
Mujeeb Ur Rehman <bcs.23440216@imsciences.edu.pk>
2026-06-30 16:44:18 +0500

    fix: explicit production server mapping

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  // This automatically configures TanStack Start and resolves the entry points
+  tanstackStart: {
+    server: {
+      entry: "server"
+    },
+  },
+  nitro: {
+    preset: "vercel",
+  }
 });
